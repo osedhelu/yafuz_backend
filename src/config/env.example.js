adfb3728c1cef7ff4edf87f7cfc5e5d58f288f70
osedhelu <osedhelu@gmail.com>
2021-01-14 17:07:11 -0500

    actulizacion en variables de entorno

---
+++ src/config/env.example.js
@@ -0,0 +1,19 @@
+﻿const env = {
+    PORT: 3000,
+    LOCALHOST: 'localhost',
+    HOST_DB: 'localhost',
+    NAME_DB: 'yafuz_db',
+    PASS_DB: '',
+    USER_DB: 'yafuz',
+    PORT_DB: 1433,
+    SEED_TOKEN: 'esta_es_la_mama_de_las_comtraseñas_lista_para_no_dejar_pasar_a_nafien',
+    // APIKEY: '9e86ca80d2834a6096bf9bc5e2066f07',
+    // urlAPI: 'https://api.geoapify.com/v1/geocode/reverse?',
+    // wonpiUrl_prod: 'https://production.wompi.co/v1',
+    // keyWompi_prod: 'pub_prod_SVGZnrD7QJ1OOI1gnNYc9H1LHajQdy7W',
+    
+    // wonpiUrl_test: 'https://sandbox.wompi.co/v1',
+    // keyWompi_test: 'pub_test_Dmi9bwpWYXEBVFIqJe0YdYj6BMsYatC0'
+}
+
+module.exports = {env};
