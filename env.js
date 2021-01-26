e35977448e8e4cf8a80421c7d0c9e80d65eef914
osedhelu <osedhelu@gmail.com>
2021-01-26 09:47:33 -0500

    nuevos aactualizaciones

---
+++ env.js
@@ -0,0 +1,16 @@
+﻿const env = {
+    PORT: 3000,
+    LOCALHOST: 'localhost',
+    HOST_DB: 'localhost',
+    NAME_DB: 'u437562469_yafuz',
+    PASS_DB: '',
+    USER_DB: 'admin1',
+    PORT_DB: 1433,
+    SEED_TOKEN: 'esta_es_la_mama_de_las_comtraseñas_lista_para_no_dejar_pasar_a_nafien',
+    mongodbURL_local: 'mongodb://yafuz:ADMadm1234@3.139.160.95:1451/yafuzDB?authSource=admin&readPreference=primary&gssapiServiceName=mongodb&appname=MongoDB%20Compass&ssl=false',
+
+    EMAIL_USER: 'peverything2016@gmail.com',
+    EMAIL_PASS: 'peverything2021',
+}
+
+module.exports = {env};
