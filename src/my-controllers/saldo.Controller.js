21e6c06c10fd7c1a7e7310e162d9ef1fbcf6b301
osedhelu <osedhelu@gmail.com>
2021-03-12 14:58:25 -0500

    Merge branch 'samuel' of github.com:osedhelu/senaBaclend into oscar

--- src/my-controllers/saldo.Controller.js
+++ src/my-controllers/saldo.Controller.js
@@ -1,9 +1,25 @@
 const { SaldosModels } = require("../my-models");
 
 let SaldosController = async (socket, user, io) => {
+	socket.join(user._id)
 	let model = new SaldosModels(user._id)
+
 	socket.emit('getSaldos', await model.saldo())
-	
+	socket.on('realizarPago', async({id, valor}, callback) => {
+		try {
+			let resp = await model.transferrirSaldo(id,valor, 'recarga');
+			if(resp) {
+				socket.emit('getSaldos', await model.saldo())
+				console.log(await model.saldo());
+				callback(resp)
+			}
+		} catch (err) {
+			callback(err)
+		}
+	})
+
+
+	socket.emit('getTransaccionAll',)
 }
 
 module.exports = {SaldosController}
